b5958ce3390b860771576facfba44146161ccd07
aditya <jaditya98@gmail.com>
2024-06-16 23:11:29 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: var(--color-brand-600);
+  color: var(--color-brand);
+  cursor: pointer;
+`;
+
+const App = () => {
+  return <div>Hello</div>;
+};
+
+export default App;
