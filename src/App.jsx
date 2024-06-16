59e80082c8f2cea05785bb3135c98572e5bbf2e4
aditya <jaditya98@gmail.com>
2024-06-17 04:09:19 +0530

    add sidebar and main navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
-import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+// import Account from "./pages/Account";
+// import Bookings from "./pages/Bookings";
+// import Cabins from "./pages/Cabins";
+// import Login from "./pages/Login";
+// import Settings from "./pages/Settings";
+// import PageNotFound from "./pages/PageNotFound";
+// import Users from "./pages/Users";
 import GlobalStyles from "./styles/GlobalStyles";
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: var(--color-brand-600);
-  color: var(--color-brand);
-  cursor: pointer;
-`;
-
-const StyledApp = styled.div`
-  background-color: orangered;
-  padding: 20px;
-`;
+import AppLayout from "./ui/AppLayout";
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <Button onClick={() => alert("Hello")}>Button 1</Button>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            {/* <Route path="account" element={<Account />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="users" element={<Users />} /> */}
+          </Route>
+          {/* <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,19 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+// import Heading from "../ui/Heading";
+// import Row from "../ui/Row";
 
-function Dashboard() {
-  return (
-    <Row type="horizontal">
-      <Heading as="h1">Dashboard</Heading>
-      <p>TEST</p>
-    </Row>
-  );
-}
+// function Dashboard() {
+//   return (
+//     <Row type="horizontal">
+//       <Heading as="h1">Dashboard</Heading>
+//       <p>TEST</p>
+//     </Row>
+//   );
+// }
+
+// export default Dashboard;
+
+const Dashboard = () => {
+  return <div>Dashboard</div>;
+};
 
 export default Dashboard;
