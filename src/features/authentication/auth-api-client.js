08406e51c53978451c357c0f2497863dc4aea162
aditya <jaditya98@gmail.com>
2024-06-27 23:46:47 +0530

    add signup api request

--- src/features/authentication/auth-api-client.js
+++ src/features/authentication/auth-api-client.js
@@ -2,11 +2,11 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createEditCabin } from "../../services/apiCabins";
 import toast from "react-hot-toast";
 
-export const useEditCabin = () => {
+export const useLogin = () => {
   const queryClient = useQueryClient();
 
   const { mutate: login, isLoading } = useMutation({
-    mutationFn: ({ user }) => createEditCabin(user),
+    mutationFn: ({ user }) => loginUser(user),
 
     onSuccess: () => {
       toast.success("User login successfully");
@@ -22,4 +22,25 @@ export const useEditCabin = () => {
   });
 
   return { login, isLoading };
+};
+export const useCreateUser = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate: createUser, isLoading } = useMutation({
+    mutationFn: ({ user }) => createUser(user),
+
+    onSuccess: () => {
+      toast.success("User created successfully");
+
+      queryClient.invalidateQueries({
+        queryKey: ["users"],
+      });
+    },
+
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  return { createUser, isLoading };
 };
